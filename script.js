78dd09baec3dcbe991e70d49037366b45b1ca025
razeen-bahadoor <46553643+razeen-bahadoor@users.noreply.github.com>
2023-10-29 19:02:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    const body = document.querySelector("body");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "fas fa-heart";
+    heart.style.left = (Math.random() * 100)+"vw";
+    heart.style.animationDuration = (Math.random()*3)+2+"s"
+    body.appendChild(heart);
+}
+setInterval(createHeart,100);
+setInterval(function name(params) {
+    var heartArr = document.querySelectorAll(".fa-heart")
+    if (heartArr.length > 200) {
+       heartArr[0].remove()
+    }
+    //console.log(heartArr);
+},100)
+
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+    alert("I'm sorry to hear that. Please take your time.");
+});
+
